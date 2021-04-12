49874fcba366a0dc5bd78eb7cbe57dc1dab4e6a2
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-04-11 23:08:31 -0500

    add clean plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -14,7 +13,7 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
-    mode: 'production',
+    mode: 'development',
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -79,13 +78,5 @@ module.exports = {
             filename: 'assets/[name].[contenthash].css'
         }),
         new Dotenv(),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
-    }
 }
