4f5481ede3a772da5067574eecb51df721b64ed4
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-04-11 20:07:55 -0500

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -18,7 +19,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
         ]
     },
     plugins: [
@@ -27,5 +35,6 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html',
         }),
+        new MiniCssExtractPlugin(),
     ]
 }
